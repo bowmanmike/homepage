69d9fdf492239696f78b8e3d9c19cf64d943d862
Mike Bowman <mbowman2@gmail.com>
2021-07-07 23:04:05 -0400

    Add image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "homepage",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 };
