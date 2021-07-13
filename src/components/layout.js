fa347182be601a59cc8dcc3c6509eba84c0595d5
Mike Bowman <mbowman2@gmail.com>
2021-07-12 20:46:50 -0400

    Add tailwind and swap styles to use tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,5 @@
 import * as React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle
-} from "./layout.module.css"
 
 const Layout = ({pageTitle, children}) => {
   const data = useStaticQuery(graphql`
@@ -20,17 +12,17 @@ const Layout = ({pageTitle, children}) => {
     }
     `)
   return (
-    <main className={container}>
+    <main className="m-auto max-w-screen-sm font-sans">
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
+      <p className="text-5xl text-gray-600 font-bold">{data.site.siteMetadata.title}</p>
       <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-          <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+        <ul className="flex list-none pl-0">
+          <li className="pr-8"><Link to="/" className="text-black">Home</Link></li>
+          <li className="pr-8"><Link to="/about" className="text-black">About</Link></li>
+          <li className="pr-8"><Link to="/blog" className="text-black">Blog</Link></li>
         </ul>
       </nav>
-      <h1 className={heading}>{pageTitle}</h1>
+      <h1 className="text-purple-800">{pageTitle}</h1>
       {children}
     </main>
   )
