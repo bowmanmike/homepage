03dd7f2dd7285a26e527c2fd468ca0a083492042
Mike Bowman <mike@mikebowman.dev>
2023-04-11 20:06:34 -0400

    Initial commit

---
+++ lib/homepage_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule HomepageWeb.Layouts do
+  use HomepageWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/homepage_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule HomepageWeb.PageHTML do
+  use HomepageWeb, :html
+
+  embed_templates "page_html/*"
+end
